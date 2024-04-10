9df049a839d3a6288741d3df10f5411b676696ee
Jay Mahato <jaymahato10125@gmail.com>
2024-04-11 02:37:35 +0530

    done

---
+++ rec.cpp
@@ -0,0 +1,25 @@
+#include<iostream>   //fibaoacci series using recursion
+using namespace std;
+
+int fibaoacci(int n)
+{
+    if(n==1)
+    {
+        return 0;
+    }
+    if(n==2){
+        return 1;
+    }
+    else
+    {
+        return fibaoacci(n-1)+fibaoacci(n-2);
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    cout<<"The foctorial of the number is: "<<fibaoacci(n)<<endl;
+}
